d82cc2231253209e5ad313626be70c1d996fef5e
Jordan Falcon <j.falcon122@gmail.com>
2017-04-29 16:37:05 -0400

    More setups

---
+++ scripts/start_message.js
@@ -0,0 +1,2 @@
+var chalk = require('chalk');
+console.log(chalk.white('Starting app in dev mode...')); //eslint-disable-line no-console
---
+++ src/index.js
@@ -0,0 +1,32 @@
+// import './index.css';
+import {getProducts, deleteProduct} from './api/products';
+
+getProducts().then(result => {
+
+	let prods = "";
+
+	result.forEach(prod => {
+		if (prod) {
+			prods += `<tr>
+			<td>${prod.id}</td>
+			<td>${prod.name}</td>
+			<td>${prod.desc}</td>
+			</tr>
+			`
+		}
+	});
+
+	global.document.getElementById('products').innerHTML = prods;
+
+	// const deleteLinks = global.document.getElementsByClassName('deleteProduct');
+
+	// Array.from(deleteLinks, link => {
+	// 	link.onclick = function(event) {
+	// 		const element = event.target;
+	// 		event.preventDefault();
+	// 		deleteProduct(element.attributes["data-id"].value);
+	// 		const row = element.parentNode.parentNode;
+	// 		row.parentNode.removeChild(row);
+	// 	};
+	// });
+});
